f8ed891a685e655d1a029f7fa0a91120406ee8a8
Skuznetz <s.kuznetzov@mail.ru>
2017-07-19 19:48:47 +0300

    функция fun1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-+++
+function fun1() {
+	var rtl=document.getElementBYId('rtl').value;
+	var rtr=document.getElementBYId('rtr').value;
+	var rbl=document.getElementBYId('rbl').value;
+	var ttl=document.getElementBYId('ttl').value;
+	var ttr=document.getElementBYId('ttr').value;
+	var tbr=document.getElementBYId('tbr').value;
+	var tbl=document.getElementBYId('tbl').value;
+	var rbr=document.getElementBYId('rbr').value;
+}
