d74a7821442e90f9224479a4502eb1000ad1719d
Skuznetz <s.kuznetzov@mail.ru>
2017-07-26 22:00:36 +0300

    отформатируем файл

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 function fun1() {
-	var rtl=document.getElementById('rtl').value;
-	var rtr=document.getElementById('rtr').value;
-	var rbl=document.getElementById('rbl').value;
-	var rbr=document.getElementById('rbr').value;
-	var ttl=document.getElementById('ttl');
-	var ttr=document.getElementById('ttr');
-	var tbr=document.getElementById('tbr');
-	var tbl=document.getElementById('tbl');
-	
-
-	var block=document.getElementById('block') ;
+    var rtl = document.getElementById('rtl').value;
+    var rtr = document.getElementById('rtr').value;
+    var rbl = document.getElementById('rbl').value;
+    var rbr = document.getElementById('rbr').value;
+    var ttl = document.getElementById('ttl');
+    var ttr = document.getElementById('ttr');
+    var tbr = document.getElementById('tbr');
+    var tbl = document.getElementById('tbl');
 
 
-ttl.value = rtl;
-ttr.value = rtr;
-tbr.value = rbr;
-tbl.value = rbl;
-block.style.borderRadius = rtl + 'px ' + rtr +'px ' + rbr + 'px '+ rbl + 'px ';
+    var block = document.getElementById('block');
+
+
+    ttl.value = rtl;
+    ttr.value = rtr;
+    tbr.value = rbr;
+    tbl.value = rbl;
+    block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
 }
 
 function move() {
-	var elem = document.getElementById("myBar");
-	var width = 10;
-	var id = setInterval(frame,10);
-	function frame() {
-		if (width >= 100){
-			clearInterval(id);
-		}else {
-			width++;
-			elem.style.width = width + '%';
-			document.getElementById("label").innerHTML=width*1 +'%';
-		}
-	}
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            document.getElementById("label").innerHTML = width * 1 + '%';
+        }
+    }
 }
 
 function myMove() {
-	var elem = document.getElementById("myAnimation");
-	var pos = 0;
-	var id = setInterval(frame,10);
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    var id = setInterval(frame, 10);
 
 
-function frame() {
-	if (pos==150){
-		clearInterval(id);
-	}else {
-		pos++;
-		elem.style.top = pos+'px';
-		elem.style.left = pos + 'px';
-	}
-	}
+    function frame() {
+        if (pos == 150) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
 }
 
 function moneyConverter(valNum) {
-	document.getElementById("outputrub").innerHTML = valNum * 60;
+    document.getElementById("outputrub").innerHTML = valNum * 60;
 }
