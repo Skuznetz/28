b639dc8e00abf05a5a23d328ff53a4848eac2ed9
Skuznetz <s.kuznetzov@mail.ru>
2017-07-31 11:43:46 +0300

    функция деления

--- script.js
+++ script.js
@@ -84,3 +84,13 @@ function um() {
     resalt = num1 * num2;
     document.getElementById('out').innerHTML=result;
 }
+
+function del() {
+    var num1,num2,result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    resalt = num1 / num2;
+    document.getElementById('out').innerHTML=result;
+}
