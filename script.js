8535d071d75362fd7db20f82a2d7f00231259a19
Skuznetz <s.kuznetzov@mail.ru>
2017-08-04 09:14:09 +0300

    функция отображения меню при наведении мыши

--- script.js
+++ script.js
@@ -107,3 +107,27 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+document.getElementById('nav').onmouseover=function(event) {
+    var target=event.target;
+    if (target.className=='menu-item') {
+        var s=target.getElementsByClassName('submenu');
+        closeMenu();
+        s[0].style.display='block';
+    }
+}
+/*document.onmouseover=function(event) {
+    var target=event.target;
+    console.log(event.target);
+    if (target.className!='menu-item'&& target.className!='submenu' {
+        closeMenu();
+    })
+}
+function closeMenu() {
+    var menu=document.getElementById('nav');
+    var subm=document.getElementsByClassName('submenu');
+    for (var i=0;i<subm.length;i++) {
+        subm[j].style.display="none";
+    }
+}
+/*
