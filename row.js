cc9e40acd93c91dc56a82b511d9520bd3fc0bdcd
Skuznetz <s.kuznetzov@mail.ru>
2017-08-04 23:10:01 +0300

    форматируем файл row.js

--- row.js
+++ row.js
@@ -3,21 +3,21 @@ document.getElementById('nav').onmouseover = function(event) {
     if (target.className == 'menu-item') {
         var s = target.getElementsByClassName('submenu');
         closeMenu();
-        s[0].style.display='block';
+        s[0].style.display = 'block';
     }
 }
-document.onmouseover=function(event) {
+document.onmouseover = function(event) {
     var target = event.target;
     console.log(event.target);
-    if (target.className!='menu-item' && target.className!='submenu'){
-            closeMenu();
-        }
+    if (target.className != 'menu-item' && target.className != 'submenu') {
+        closeMenu();
+    }
 }
 
 function closeMenu() {
     var menu = document.getElementById('nav');
-    var subm=document.getElementsByClassName('submenu');
-    for (var i=0; i <subm.length; i++) {
-        subm[i].style.display="none";
+    var subm = document.getElementsByClassName('submenu');
+    for (var i = 0; i < subm.length; i++) {
+        subm[i].style.display = "none";
     }
 }
