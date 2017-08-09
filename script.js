212bcd544f9b12791c84d5ece287fefe4aed76c8
Skuznetz <s.kuznetzov@mail.ru>
2017-08-09 16:42:27 +0300

    пишем close

--- script.js
+++ script.js
@@ -135,5 +135,17 @@ function myFunction() {
         x.style.display = 'block';
     }else {
         x.style.display = 'none';
-    }
+  }
 }
+        var close = document.getElementsByClassName("closebtn");
+        var i;
+        for (i=0;i <close.length;i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement ;
+                div.style.opacity ="0";
+                setTimeout(function()) {
+                    div.style.display = "none";
+                },600);
+            }
+        }
+    
